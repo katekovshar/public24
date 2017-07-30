a2487201797f40d4200ee2ee81e67b88298ec373
Horbunov Mykhailo <miwagorbi@gmail.com>
2017-07-30 13:42:53 +0300

    Further simplified security config

--- src/main/java/com/voidaspect/public24/security/SecurityConfig.java
+++ src/main/java/com/voidaspect/public24/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.voidaspect.public24.security;
 
-import lombok.val;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 /**
  * @author mikhail.h
@@ -18,27 +15,18 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationEn
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static String REALM = "PUB24";
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/**").hasRole("USER")
-                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+                .and().httpBasic().realmName("PUB24")
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
-    }
-
-    @Bean
-    public BasicAuthenticationEntryPoint getBasicAuthEntryPoint() {
-        val entryPoint = new BasicAuthenticationEntryPoint();
-        entryPoint.setRealmName(REALM);
-        return entryPoint;
     }
 
 }
