36fe6bde3db9fc977fbd7e3968d2de14c1ac9866
Horbunov Mykhailo <miwagorbi@gmail.com>
2017-07-31 11:32:59 +0300

    Added currency enum

---
+++ src/main/java/com/voidaspect/public24/service/Currency.java
@@ -0,0 +1,25 @@
+package com.voidaspect.public24.service;
+
+/**
+ * @author mikhail.h
+ */
+public enum Currency {
+
+    USD,
+
+    EUR,
+
+    RUR,
+
+    CHF,
+
+    GBP,
+
+    PLZ,
+
+    SEK,
+
+    XAU,
+
+    CAD
+}
