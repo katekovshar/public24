93ba890a0ac93d4046a000bfb46932c54211ca83
Gorbunov Michael <miwagorbi@gmail.com>
2017-08-12 22:34:58 +0300

    Added static web content to security ignore list

--- src/main/java/com/voidaspect/public24/security/SecurityConfig.java
+++ src/main/java/com/voidaspect/public24/security/SecurityConfig.java
@@ -27,7 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS);
+        web.ignoring()
+                .antMatchers("/resources/**")
+                .antMatchers(HttpMethod.OPTIONS);
     }
 
 }
